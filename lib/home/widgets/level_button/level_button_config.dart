b502dac42634c2e0ea919619bd5f385aa0431468
VitorLamego <vitorlamegoo@gmail.com>
2021-04-20 16:43:12 -0300

    Create component LevelButton with possible configs

---
+++ lib/home/widgets/level_button/level_button_config.dart
@@ -0,0 +1,26 @@
+import 'package:DevQuiz/core/core.dart';
+
+class LevelButtonConfig {
+  static final config = {
+    "Fácil": {
+      "color": AppColors.levelButtonFacil,
+      "borderColor": AppColors.levelButtonBorderFacil,
+      "fontColor": AppColors.levelButtonTextFacil
+    },
+    "Médio": {
+      "color": AppColors.levelButtonMedio,
+      "borderColor": AppColors.levelButtonBorderMedio,
+      "fontColor": AppColors.levelButtonTextMedio
+    },
+    "Difícil": {
+      "color": AppColors.levelButtonDificil,
+      "borderColor": AppColors.levelButtonBorderDificil,
+      "fontColor": AppColors.levelButtonTextDificil
+    },
+    "Perito": {
+      "color": AppColors.levelButtonPerito,
+      "borderColor": AppColors.levelButtonBorderPerito,
+      "fontColor": AppColors.levelButtonTextPerito
+    }
+  };
+}
--- lib/home/widgets/level_button/level_button_widget.dart
+++ lib/home/widgets/level_button/level_button_widget.dart
@@ -1,8 +1,32 @@
+import 'package:DevQuiz/home/widgets/level_button/level_button_config.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LevelButtonWidget extends StatelessWidget {
+  final String label;
+
+  LevelButtonWidget({required this.label, Key? key})
+      : assert(["Fácil", "Médio", "Difícil", "Perito"].contains(label)),
+        super(key: key);
+
+  final config = LevelButtonConfig.config;
+
+  Color get color => config[label]!['color']!;
+  Color get borderColor => config[label]!['borderColor']!;
+  Color get fontColor => config[label]!['fontColor']!;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(28),
+          border: Border.all(color: borderColor),
+          color: color),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+        child: Text(label,
+            style: GoogleFonts.notoSans(color: fontColor, fontSize: 13)),
+      ),
+    );
   }
 }
