ba5420dbfb8a74c29f231fc7c46b7ba821674612
VitorLamego <vitorlamegoo@gmail.com>
2021-04-20 17:35:17 -0300

    Convert the ProgressIndicator into a global component

---
+++ lib/shared/widget/progress_indicator/progress_indicator_widget.dart
@@ -0,0 +1,18 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:flutter/material.dart';
+
+class ProgressIndicatorWidget extends StatelessWidget {
+  final double value;
+
+  const ProgressIndicatorWidget({required this.value, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearProgressIndicator(
+      value: 0.3,
+      backgroundColor: AppColors.chartSecondary,
+      valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+    );
+  }
+}
