2adf02335905e5dce51b1b3e0d1a8a2f9f7c40c3
VitorLamego <vitorlamegoo@gmail.com>
2021-04-22 16:44:59 -0300

    Create a named constructor on UserModel

--- lib/shared/models/user_model.dart
+++ lib/shared/models/user_model.dart
@@ -3,5 +3,5 @@ class UserModel {
   final String photoUrl;
   final int score;
 
-  UserModel(this.name, this.photoUrl, this.score);
+  UserModel({required this.name, required this.photoUrl, this.score = 0});
 }
