c17b7bd78b7c4646c354819750e6d0e7c79ef25d
VitorLamego <vitorlamegoo@gmail.com>
2021-04-22 16:44:37 -0300

    Add controller to HomePage

---
+++ lib/home/home_controller.dart
@@ -0,0 +1,38 @@
+import 'package:DevQuiz/core/app_images.dart';
+import 'package:DevQuiz/home/home_state.dart';
+import 'package:DevQuiz/shared/models/answer_model.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
+import 'package:DevQuiz/shared/models/quiz_model.dart';
+import 'package:DevQuiz/shared/models/user_model.dart';
+
+class HomeController {
+  HomeState state = HomeState.empty;
+
+  UserModel? user;
+  List<QuizModel>? quizzes;
+
+  void getUser() {
+    user = UserModel(
+        name: "Vitor Lamego",
+        photoUrl: 'https://avatars.githubusercontent.com/u/54643464?v=4');
+  }
+
+  void getQuizzes() {
+    quizzes = [
+      QuizModel(
+          title: "Princípios Básicos do Flutter",
+          questions: [
+            QuestionModel(title: "O que o Flutter faz ?", answers: [
+              AnswerModel(title: "Linguagem de programação"),
+              AnswerModel(
+                  title: "Framework de desenvolvimento mobile usando Dart",
+                  isRight: true),
+              AnswerModel(title: "Nova tecnologia de machine learning"),
+              AnswerModel(title: "Framework Backend")
+            ])
+          ],
+          image: AppImages.blocks,
+          level: Level.facil)
+    ];
+  }
+}
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:DevQuiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:DevQuiz/home/home_controller.dart';
 import 'package:DevQuiz/home/widgets/appbar/appbar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
@@ -11,10 +13,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(),
+      appBar: AppBarWidget(user: controller.user!),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
@@ -35,11 +38,9 @@ class _HomePageState extends State<HomePage> {
                 crossAxisCount: 2,
                 crossAxisSpacing: 16,
                 mainAxisSpacing: 16,
-                children: [
-                  QuizCardWidget(),
-                  QuizCardWidget(),
-                  QuizCardWidget()
-                ],
+                children: controller.quizzes!
+                    .map((quiz) => QuizWidget(title: quiz.title))
+                    .toList(),
               ),
             )
           ],
