f0c253d15a6a0227d1c51bf7b3a8df8d56dd4f55
VitorLamego <vitorlamegoo@gmail.com>
2021-04-22 17:16:00 -0300

    Add state to HomePage with ValueNotifier

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -4,23 +4,33 @@ import 'package:DevQuiz/shared/models/answer_model.dart';
 import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:DevQuiz/shared/models/quiz_model.dart';
 import 'package:DevQuiz/shared/models/user_model.dart';
+import 'package:flutter/foundation.dart';
 
 class HomeController {
-  HomeState state = HomeState.empty;
+  final stateNotifier = ValueNotifier<HomeState>(HomeState.empty);
+  set state(HomeState state) => stateNotifier.value = state;
+  HomeState get state => stateNotifier.value;
 
   UserModel? user;
   List<QuizModel>? quizzes;
 
-  void getUser() {
+  void getUser() async {
+    state = HomeState.loading;
+    await Future.delayed(Duration(seconds: 2));
     user = UserModel(
         name: "Vitor Lamego",
         photoUrl: 'https://avatars.githubusercontent.com/u/54643464?v=4');
+    state = HomeState.succes;
   }
 
-  void getQuizzes() {
+  void getQuizzes() async {
+    state = HomeState.loading;
+
+    await Future.delayed(Duration(seconds: 2));
     quizzes = [
       QuizModel(
           title: "Básico Flutter",
+          questionsAnswered: 1,
           questions: [
             QuestionModel(title: "O que o Flutter faz ?", answers: [
               AnswerModel(title: "Linguagem de programação"),
@@ -29,10 +39,19 @@ class HomeController {
                   isRight: true),
               AnswerModel(title: "Nova tecnologia de machine learning"),
               AnswerModel(title: "Framework Backend")
-            ])
+            ]),
+            QuestionModel(title: "O que o Flutter faz ?", answers: [
+              AnswerModel(title: "Linguagem de programação"),
+              AnswerModel(
+                  title: "Framework de desenvolvimento mobile usando Dart",
+                  isRight: true),
+              AnswerModel(title: "Nova tecnologia de machine learning"),
+              AnswerModel(title: "Framework Backend")
+            ]),
           ],
           image: AppImages.blocks,
           level: Level.facil)
     ];
+    state = HomeState.succes;
   }
 }
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:DevQuiz/core/core.dart';
 import 'package:DevQuiz/home/home_controller.dart';
+import 'package:DevQuiz/home/home_state.dart';
 import 'package:DevQuiz/home/widgets/appbar/appbar_widget.dart';
 import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
@@ -16,46 +18,58 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
+    super.initState();
     controller.getUser();
     controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(user: controller.user!),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                LevelButtonWidget(label: "Fácil"),
-                LevelButtonWidget(label: "Médio"),
-                LevelButtonWidget(label: "Difícil"),
-                LevelButtonWidget(label: 'Perito')
-              ],
-            ),
-            SizedBox(height: 24),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                children: controller.quizzes!
-                    .map((quiz) => QuizCardWidget(
-                        title: quiz.title,
-                        picture: quiz.image,
-                        percentage:
-                            quiz.questionsAnswered / quiz.questions.length,
-                        progress:
-                            "${quiz.questionsAnswered} de ${quiz.questions.length}"))
-                    .toList(),
+    if (controller.state == HomeState.succes)
+      return Scaffold(
+        appBar: AppBarWidget(user: controller.user!),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  LevelButtonWidget(label: "Fácil"),
+                  LevelButtonWidget(label: "Médio"),
+                  LevelButtonWidget(label: "Difícil"),
+                  LevelButtonWidget(label: 'Perito')
+                ],
               ),
-            )
-          ],
+              SizedBox(height: 24),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  children: controller.quizzes!
+                      .map((quiz) => QuizCardWidget(
+                          title: quiz.title,
+                          picture: quiz.image,
+                          percentage:
+                              quiz.questionsAnswered / quiz.questions.length,
+                          progress:
+                              "${quiz.questionsAnswered} de ${quiz.questions.length}"))
+                      .toList(),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
         ),
       ),
     );
