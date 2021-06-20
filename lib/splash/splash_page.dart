9bcb39613f712852a5f2ef18be94204e86a8bbcf
VitorLamego <vitorlamegoo@gmail.com>
2021-06-20 11:04:40 -0300

    Add Future Delay in SplashScreen

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_images.dart';
@@ -5,6 +6,9 @@ import 'package:DevQuiz/core/app_images.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2)).then((_) => Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomePage())));
+
     return Scaffold(
       body: Container(
           decoration: BoxDecoration(gradient: AppGradients.linear),
